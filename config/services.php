94b9b3b25e24fce3af569d1fa4effc071d064351
Georgi Georgiev <vuryss@gmail.com>
2025-08-20 14:32:44 +0300

    fix: Service dependency injection

--- config/services.php
+++ config/services.php
@@ -11,13 +11,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Vuryss\\DoctrineLazyJsonOdm\\', __DIR__.'/../src/');
 
-    $services->alias(
-        Vuryss\DoctrineLazyJsonOdm\Serializer\SerializerInterface::class,
-        Vuryss\DoctrineLazyJsonOdm\Serializer\Serializer::class,
-    );
+    $services
+        ->alias(
+            Vuryss\DoctrineLazyJsonOdm\Serializer\SerializerInterface::class,
+            Vuryss\DoctrineLazyJsonOdm\Serializer\Serializer::class,
+        )
+        ->public();
 
-    $services->alias(
-        Vuryss\DoctrineLazyJsonOdm\TypeMapper\TypeMapperInterface::class,
-        Vuryss\DoctrineLazyJsonOdm\TypeMapper\TypeMapper::class,
-    );
+    $services
+        ->alias(
+            Vuryss\DoctrineLazyJsonOdm\TypeMapper\TypeMapperInterface::class,
+            Vuryss\DoctrineLazyJsonOdm\TypeMapper\TypeMapper::class,
+        )
+        ->public();
 };
